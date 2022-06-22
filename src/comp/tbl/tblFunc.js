423129f2eccc9dd95c22e1c6b1fcda31a93832bd
sali12 <sali12@i2cinc.com>
2022-06-22 13:37:23 +0500

    Func vs Class

--- src/comp/tbl/tblFunc.js
+++ src/comp/tbl/tblFunc.js
@@ -2,26 +2,48 @@ import React, {useEffect, useState} from "react";
 
 const TableFunc = () => {
 
-    const [arr, setArr] = useState([]);
+    const [completeData, setCompleteData] = useState([]);
+    const [data, setData] = useState([]);
+    const [symblData, setSymblData] = useState(null);
+    const [textField, setTextField] = useState("");
 
     useEffect(()=>{
         fetch("https://api2.binance.com/api/v3/ticker/24hr")
             .then(resp => resp.json())
-            .then(data => setArr(data.slice(0,3)))
+            .then((data) => {
+                setCompleteData(data);
+            });
     },[])
 
+    useEffect(() => {
+        fetchStuff();
+    }, [completeData])
 
     function fetchStuff() {
         let tblData = document.getElementById("tbl");
         let lastRow = tblData.rows.length - 1;
+        if(lastRow == null || lastRow == undefined || lastRow == 0){
+            setData(completeData.slice(0,3));
+        }
+        else{
+            setData(completeData.slice(0, lastRow+3));
+        }
+    }
 
-
+    const fetchSymbol = () =>{
+        var url = "https://api2.binance.com/api/v3/ticker/24hr?symbol=";
+        if(textField != ""){
+            url = url+textField;
+            fetch(url)
+                .then(resp => resp.json())
+                .then(data => setSymblData(data));
+        }
     }
 
     return(
         <>
             <h1>Functional Component</h1>
-            <table id="tbl" onClick={fetchStuff}>
+            <table id="tbl">
                 <tbody>
                 <tr>
                     <th>Index</th>
@@ -30,7 +52,7 @@ const TableFunc = () => {
                     <th>Count</th>
                 </tr>
                 {
-                    arr.map((obj, index) => (
+                    data.map((obj, index) => (
                         <tr key={index}>
                             <td>{index}</td>
                             <td>{obj.symbol}</td>
@@ -41,14 +63,33 @@ const TableFunc = () => {
                 }
                 </tbody>
             </table>
-            {/*<br/>*/}
-            {/*<div>*/}
-            {/*    Symbol : <input type={"text"} id={"txt"}/>*/}
-            {/*    <input type="button" value={"Submit"}/>*/}
-            {/*    {*/}
-
-            {/*    }*/}
-            {/*</div>*/}
+            <br/>
+            <div>
+                Symbol : <input type="text" id="txt" onChange={(event)=> setTextField(event.target.value)}/>
+                <input type="button" onClick={()=>{
+                    fetchSymbol();
+                }} value={"Submit"}/>
+                {
+                    symblData?(
+                        <table id="fetchTbl">
+                            <tbody>
+                            <tr>
+                                <th>Symbol</th>
+                                <th>Price Range</th>
+                                <th>Count</th>
+                            </tr>
+                            {
+                                <tr>
+                                    <td>{symblData.symbol}</td>
+                                    <td>{symblData.priceChange}</td>
+                                    <td>{symblData.count}</td>
+                                </tr>
+                            }
+                            </tbody>
+                        </table>
+                    ):null
+                }
+            </div>
         </>
     )
 
