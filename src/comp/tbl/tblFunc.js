75f89cbb6431b48e520a9fd87601224cc4424b5f
sali12 <sali12@i2cinc.com>
2022-06-22 18:51:38 +0500

    Func vs Class

--- src/comp/tbl/tblFunc.js
+++ src/comp/tbl/tblFunc.js
@@ -1,4 +1,6 @@
-import React, {useEffect, useState} from "react";
+import React, {memo, useEffect, useState} from "react";
+import tblClass from "./tblClass";
+import axios from "axios";
 
 const TableFunc = () => {
 
@@ -7,17 +9,56 @@ const TableFunc = () => {
     const [symblData, setSymblData] = useState(null);
     const [textField, setTextField] = useState("");
 
+    //componentDidMount
     useEffect(()=>{
+
+
         fetch("https://api2.binance.com/api/v3/ticker/24hr")
             .then(resp => resp.json())
-            .then((data) => {
-                setCompleteData(data);
-            });
+            .then(data => this.setState({dataForTbl:data.slice(0,3)}));
+
+        axios.get("https://api2.binance.com/api/v3/ticker/24hr")
+            .then(resp => setCompleteData(resp.data) );
+
+
+        fetch('https://jsonplaceholder.typicode.com/posts', {
+            method: 'POST',
+            body: JSON.stringify({
+                title: 'test',
+                body: 'body',
+                userId: 'testuser'
+            }),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            }
+        })
+        .then(response => response.json())
+        .then(json => {
+           // console.log('Inserted successfully,  response: ' + JSON.stringify(json));
+        });
+
+        axios.post('https://jsonplaceholder.typicode.com/posts', {title: 'test', body: 'body', userId: 'testuser'})
+            .then(resp => console.log(resp.data));
+
     },[])
 
+    //componentDidUpdate
     useEffect(() => {
         fetchStuff();
     }, [completeData])
+
+    //componentWillUnmount
+    useEffect(()=>{
+        return  () => {
+
+        }
+    },[])
+
+    //render
+    useEffect(()=>{
+
+    })
+
 
     function fetchStuff() {
         let tblData = document.getElementById("tbl");
@@ -40,7 +81,28 @@ const TableFunc = () => {
         }
     }
 
+    const symblFunction = () => (
+        symblData?(
+            <table id="fetchTbl">
+                <tbody>
+                <tr>
+                    <th>Symbol</th>
+                    <th>Price Range</th>
+                    <th>Count</th>
+                </tr>
+                {
+                    <tr>
+                        <td>{symblData.symbol}</td>
+                        <td>{symblData.priceChange}</td>
+                        <td>{symblData.count}</td>
+                    </tr>
+                }
+                </tbody>
+            </table>
+        ):null
+    )
     return(
+
         <>
             <h1>Functional Component</h1>
             <table id="tbl">
@@ -69,30 +131,13 @@ const TableFunc = () => {
                 <input type="button" onClick={()=>{
                     fetchSymbol();
                 }} value={"Submit"}/>
+                <br/>
                 {
-                    symblData?(
-                        <table id="fetchTbl">
-                            <tbody>
-                            <tr>
-                                <th>Symbol</th>
-                                <th>Price Range</th>
-                                <th>Count</th>
-                            </tr>
-                            {
-                                <tr>
-                                    <td>{symblData.symbol}</td>
-                                    <td>{symblData.priceChange}</td>
-                                    <td>{symblData.count}</td>
-                                </tr>
-                            }
-                            </tbody>
-                        </table>
-                    ):null
+                    symblFunction()
                 }
             </div>
         </>
     )
-
 }
 
-export default TableFunc;
+export default memo(TableFunc);
